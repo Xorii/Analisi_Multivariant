f6f23684762b3304953243c4e8e6299aa4a4622a
Shoo03 <161711836+Shoo03@users.noreply.github.com>
2024-02-29 12:05:27 +0100

    Update Analisi Univariant.R
    He tret la part de lectura de dades

--- Analisi Univariant.R
+++ Analisi Univariant.R
@@ -1,13 +1,3 @@
-library(readxl)
-df <- read_excel("Dades.xlsx")
-
-df <- as.data.frame(df[2:ncol(df)])
-
-df[,c("kids","teens","NOf")] <- sapply(df[,c("kids","teens","nOf")],as.numeric)
-
-df[,names(df)[sapply(df,class)=="character"]] <- 
-  lapply(df[,names(df)[sapply(df,class)=="character"]],as.factor)
-
 attach(df)
 
 
